3d64f6ddab49c83a37e69096db7e04d7db88f258
realRJ10 <121657363+realRJ10@users.noreply.github.com>
2023-03-17 22:55:32 +0330

    added conversion

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ date();
 
 function showTemp(response) {
   let temprature = document.querySelector("#temp");
-  temprature.innerHTML = `${Math.round(response.data.main.temp)} °C`;
+  temprature.innerHTML = Math.round(response.data.main.temp);
   let head = document.querySelector("h1");
   head.innerHTML = response.data.name;
   let humidity = document.querySelector("#humidity");
@@ -41,6 +41,8 @@ function showTemp(response) {
   );
   let description = document.querySelector("#description");
   description.innerHTML = response.data.weather[0].description;
+  celsius = response.data.main.temp;
+  celsiusFeel = response.data.main.feels_like;
 }
 
 function changeCity(event) {
@@ -49,12 +51,12 @@ function changeCity(event) {
   let city = input.value;
   let apiKey = "f3009e4852fa0a079dab291dabf020c4";
   let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  axios.get(weatherUrl).then(showTemp);
+  axios.get(weatherUrl).then(showTemp, changeTempToF);
 }
 
 function showCurrentTemp(response) {
   let temprature = document.querySelector("#temp");
-  temprature.innerHTML = `${Math.round(response.data.main.temp)} °C`;
+  temprature.innerHTML = Math.round(response.data.main.temp);
   let head = document.querySelector("h1");
   head.innerHTML = response.data.name;
   let humidity = document.querySelector("#humidity");
@@ -70,12 +72,14 @@ function showCurrentTemp(response) {
   );
   let description = document.querySelector("#description");
   description.innerHTML = response.data.weather[0].description;
+  celsius = response.data.main.temp;
+  celsiusFeel = response.data.main.feels_like;
 }
 
 function currentTemp(position) {
   let apiKey = "f3009e4852fa0a079dab291dabf020c4";
   let currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(currentWeatherUrl).then(showCurrentTemp);
+  axios.get(currentWeatherUrl).then(showCurrentTemp, changeTempToF);
 }
 
 function getLocBottun(event) {
@@ -83,11 +87,36 @@ function getLocBottun(event) {
   navigator.geolocation.getCurrentPosition(currentTemp);
 }
 
+function changeTempToF(event) {
+  event.preventDefault();
+  let temprature = document.querySelector("#temp");
+  temprature.innerHTML = Math.round((celsius * 9) / 5 + 32);
+  let feelLike = document.querySelector("#feel");
+  feelLike.innerHTML = `${(celsiusFeel * 9) / 5 + 32} °F`;
+}
+
+function changeTempToC(event) {
+  event.preventDefault();
+  let temprature = document.querySelector("#temp");
+  temprature.innerHTML = Math.round(celsius);
+  let feelLike = document.querySelector("#feel");
+  feelLike.innerHTML = `${celsiusFeel} °C`;
+}
+
+let celsius = null;
+let celsiusFeel = null;
+
 let currentButton = document.querySelector("#current");
 currentButton.addEventListener("click", getLocBottun);
 
 let button = document.querySelector("#button");
 button.addEventListener("click", changeCity);
+
+let changeToF = document.querySelector("#toF");
+changeToF.addEventListener("click", changeTempToF);
+
+let changeToC = document.querySelector("#toC");
+changeToC.addEventListener("click", changeTempToC);
 
 navigator.geolocation.getCurrentPosition(currentTemp);
 showCurrentTemp();
